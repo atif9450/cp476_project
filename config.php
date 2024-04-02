1c88c52c4e9a207f173c578ef447c5df8ca53a26
GurmanS2000 <152099403+GurmanS2000@users.noreply.github.com>
2024-04-02 19:01:56 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuration
+$servername = ""; // Change this if your MySQL server is hosted elsewhere
+$username = ""; // Your MySQL username
+$password = ""; // Your MySQL password
+$dbname = "CP476_PROJECT";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
